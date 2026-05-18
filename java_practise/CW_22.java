f0bda3ddcca5e93e329abf9f39b847c5baff7265
chaitanyasanodiya12-hub <chaitanyasanodiya12@gmail.com>
2026-05-18 20:42:28 +0530

    Add files via upload
    i have pushed  22nd file and il be conting this further with time !!

---
+++ java_practise/CW_22.java
@@ -0,0 +1,17 @@
+public class CW_22 {
+
+    public static void main(String[] args) {
+
+int[] marks = {34,54,56,76,44};
+        System.out.println(marks[3]);
+
+
+
+        }
+
+
+
+        }
+
+
+
