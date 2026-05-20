2708671152dc192d66f9ac6bfe43f6185a863235
Chaitanya_Sanodiya <chaitanyasanodiya12@gmail.com>
2026-05-20 20:55:09 +0530

    Add files via upload
    i have completed varargs !! 20th may 2026 !!

---
+++ java_practise/varargs.java
@@ -0,0 +1,17 @@
+public class varargs {
+
+
+  public  static int sum(int x , int ...arr) {
+           int result = x ;
+
+        for (int a : arr) {
+            result += a;
+        }
+        return result;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(5,7,3,5));
+    }
+}
