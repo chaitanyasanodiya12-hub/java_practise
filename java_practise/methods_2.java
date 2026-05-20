f5ff6e5cffe3076b586c3988cdd3d0ddf2c19fb0
Chaitanya_Sanodiya <chaitanyasanodiya12@gmail.com>
2026-05-20 13:06:53 +0530

    Add files via upload
    Done WIth Methods in java !!! pushed by  my brother !!

---
+++ java_practise/methods_2.java
@@ -0,0 +1,15 @@
+
+
+
+          public class methods_2 {
+              static void telljoke(){
+                  System.out.println("i invented a new word\n" +
+                          "palgrism");
+
+              }
+
+              public static void main(String[] args) {
+                  telljoke();
+              }
+
+}
